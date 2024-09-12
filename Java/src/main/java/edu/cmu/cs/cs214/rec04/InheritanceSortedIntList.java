f7df8f2cb3e3fee5ddca407b261ff8550524312b
jameswang000 <jameswa2@andrew.cmu.edu>
2024-09-11 21:48:50 -0400

    Implemented InheritanceSortedIntList class so that it extends SortedIntList

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,56 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private int addCount; 
 
+    /**
+     * Creates a new instance of a InheritanceSortedIntList with
+     * the count of attempted insertions starting at zero.
+     *
+     * @return The newly created InheritanceSortedIntList object
+     */
+    public InheritanceSortedIntList() {
+        super();
+        this.addCount = 0;
+    }
+
+    /**
+     * Adds the specified int to the list and increases the count
+     * attempted insertions by 1
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        boolean addSuccessful = super.add(num);
+        this.addCount = this.addCount + 1;
+        return addSuccessful;
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     * Calls add() on each element of list with each add() call
+     * increasing the count of attempted insertions by 1
+     *                                          
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean addSuccessful = super.addAll(list);
+        return addSuccessful;
+    }
+
+    /**
+     * Gets the total number of elements added to the list so far 
+     *                                          
+     * @return An integer representing the number of attempted insertions
+     * so far
+     */
+    public int getTotalAdded() {
+        return this.addCount;
+    }
 }
